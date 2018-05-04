4dabe2ded7fe7206548ee1a648fed33dbba76bf7
Dhinuskrabs <dhinuskrabs@gmail.com>
2018-05-04 00:51:41 -0700

    this is the  commit after long time

---
+++ src/main/java/com/qsp/mavenpractice/com/qsp/mavenpractice/App.java
@@ -0,0 +1,14 @@
+package com.qsp.mavenpractice.com.qsp.mavenpractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("dhinus krabs pjoect");
+    }
+}
